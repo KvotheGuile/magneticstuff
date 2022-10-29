0e8c3c05818209cbf0c73acc7229ccf548496e74
Kvothe Guile <112582839+KvotheGuile@users.noreply.github.com>
2022-10-28 20:17:28 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(input.magneticForce(Dimension.Strength))
+})
 basic.forever(function () {
-	
+    if (input.magneticForce(Dimension.Strength) < 200) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . . . .
+            # # # # #
+            # . # . #
+            `)
+        music.playTone(988, music.beat(BeatFraction.Whole))
+    } else {
+        basic.clearScreen()
+    }
 })
